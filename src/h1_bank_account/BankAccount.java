7f5c9d701275691688d904d7e31992c1ff70cf1a
TDoroshenko <tetyanadoroshenko@gmail.com>
2019-07-15 12:06:54 +0300

    update

--- src/h1_bank_account/BankAccount.java
+++ src/h1_bank_account/BankAccount.java
@@ -1,8 +1,8 @@
-package h1_bank_account;
+import java.math.BigDecimal;
 
 public class BankAccount {
     private long accountNumber;
-    private double balance;
+    private BigDecimal balance;
 
     public void setAccountNumber(long accountNumber) {
         this.accountNumber = accountNumber;
@@ -12,28 +12,32 @@ public class BankAccount {
         return accountNumber;
     }
 
-    public void setBalance(double balance){
+    public void setBalance(BigDecimal balance) {
         this.balance = balance;
     }
 
-    public double getBalance() {
+    public BigDecimal getBalance() {
         return balance;
     }
 
-    public double credit(double addSum) {
-        balance = balance + addSum;
+    public BigDecimal credit(double addSum) {
+        balance = balance.add(BigDecimal.valueOf(addSum));
         return balance;
     }
 
-    public double debit(double substractSum) {
-        if (substractSum > balance) System.out.println("Сумма снятия больше чем остаток на счету!");
-        balance = balance - substractSum;
+    public BigDecimal debit(double substractSum) {
+        if (balance.subtract(BigDecimal.valueOf(substractSum)).compareTo(BigDecimal.valueOf(0)) < 0) {
+            System.out.println("You don't have enough money for the operation.");
+        } else {
+            balance = balance.subtract(BigDecimal.valueOf(substractSum));
+        }
         return balance;
     }
 
-    BankAccount () {}
+    BankAccount() {
+    }
 
-    BankAccount (long accountNumber, double balance) {
+    BankAccount(long accountNumber, BigDecimal balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
@@ -43,9 +47,6 @@ public class BankAccount {
                 this.accountNumber + ", баланс: " +
                 this.balance;
     }
-
-
-
 
 
 }
--- src/h1_bank_account/Main.java
+++ src/h1_bank_account/Main.java
@@ -1,10 +1,21 @@
-package h1_bank_account;
+/*
+Из предыдущего ДЗ: Создать класс BankAccount который будет иметь два поля
+accountNumber и balance.
+Реализовать в классе методы setBalance() который будет задавать баланс ,
+credit() метод должен начислять средства на счет, debit() - снимать средства со  счета.
+Реализовать условие что если сумма снятия больше чем остаток на счету -
+вывести сообщение : “Сумма снятия больше чем остаток на счету!” .
+Создать обьект класса и проверить работу программы.
+ */
+
+import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount(1001, 10.3);
-        bankAccount.debit(15.0);
-        bankAccount.credit(1.1);
+        BankAccount bankAccount = new BankAccount(1001, BigDecimal.valueOf(13.00));
+        bankAccount.debit(7.00);
+        bankAccount.credit(1.18);
         System.out.println(bankAccount.toString());
     }
 }
+
